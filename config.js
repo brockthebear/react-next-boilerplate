c623e3563b974ccbb9c97065c3c576364cede142
Brock Boren <brock.boren@gmail.com>
2019-01-23 12:20:17 -0600

    Create custom App and Document components.
    Replace default App and Document components provided by Next
    and instead use _app.js and _document.js

---
+++ config.js
@@ -0,0 +1,1 @@
+const endpoint = 'http://localhost:4444';
---
+++ pages/_document.js
@@ -0,0 +1,26 @@
+import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+export default class MyDocument extends Document {
+  // load styles before initial render so that page does not initially render without any styling.
+  // in other words, this prevents a flicker of the screen after render.
+  // unique to Next.js (https://github.com/zeit/next.js/#custom-document)
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>{this.props.styleTags}</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
